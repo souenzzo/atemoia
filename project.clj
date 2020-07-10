05d4900cca500c1911e21b5bdec21f7c38110b19
Enzzo Cavallo <souenzzo@gmail.com>
2020-07-10 18:06:58 -0300

    ix

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject br.com.souenzzo/atemoia "0.1.0"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.deps.alpha "0.8.695"]
-                 [io.pedestal/pedestal.jetty "0.5.8"]
-                 [io.pedestal/pedestal.service "0.5.8"]]
-  :min-lein-version "2.0.0"
-  :main br.com.souenzzo.atemoia)
--- src/br/com/souenzzo/atemoia.clj
+++ src/br/com/souenzzo/atemoia.clj
@@ -1,11 +1,9 @@
 (ns br.com.souenzzo.atemoia
   (:require [io.pedestal.http :as http]
-            [clojure.edn :as edn]
-            [clojure.pprint :as pp]))
+            [clojure.edn :as edn]))
 
 (defn index
   [req]
-  (prn (keys req))
   {:body    "ok"
    :headers {"Content-Type" "text/plain"}
    :status  200})
@@ -15,6 +13,7 @@
 
 (def service-map
   {::http/port   port
+   ::http/host   "0.0.0.0"
    ::http/routes routes
    ::http/type   :jetty
    ::http/join?  false})
@@ -22,27 +21,13 @@
 (defonce state
          (atom nil))
 
-(defn debug
-  [x y]
-  (prn y)
-  x)
-
 (defn -main
   [& _]
-  (prn [:starting!!])
-  (pp/pprint (into {} (System/getenv)))
   (swap! state
          (fn [st]
-           (prn :Ok)
            (when st
-             (prn :sopt)
              (http/stop st))
-           (prn :xxx)
            (-> service-map
-               (doto (debug :a))
                http/default-interceptors
-               (doto (debug :b))
                http/create-server
-               (doto (debug :c))
-               http/start
-               (doto pp/pprint)))))
+               http/start))))
