2009ffc3336cd038a59ab752eae2f7641b6db3f4
Enzzo Cavallo <souenzzo@gmail.com>
2020-07-10 16:54:49 -0300

    WIP

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: clojure -m br.com.souenzzo.atemoia
---
+++ src/br/com/souenzzo/atemoia.clj
@@ -0,0 +1,32 @@
+(ns br.com.souenzzo.atemoia
+  (:require [io.pedestal.http :as http]
+            [clojure.edn :as edn]))
+
+(defn index
+  [req]
+  {:body    "ok"
+   :headers {"Content-Type" "text/plain"}
+   :status  200})
+
+(def routes `#{["/" :get index]})
+(def port (edn/read-string (System/getenv "PORT")))
+
+(def service-map
+  {::http/port   port
+   ::http/routes routes
+   ::http/type   :jetty
+   ::http/join?  false})
+
+(defonce state
+         (atom nil))
+
+(defn -main
+  [& _]
+  (swap! state
+         (fn [st]
+           (when st
+             (http/stop st))
+           (-> service-map
+               http/default-interceptors
+               http/create-server
+               http/start))))
