ac5d9000749c6487129b4390c9a4edb4bc51e0e3
Enzzo Cavallo <souenzzo@gmail.com>
2020-07-13 08:51:01 -0300

    +debug

--- src/br/com/souenzzo/atemoia.clj
+++ src/br/com/souenzzo/atemoia.clj
@@ -55,12 +55,16 @@
 
 (defn -main
   [& _]
+  (prn :-main)
   (swap! state
          (fn [st]
+           (prn :st)
            (when st
+             (prn :stop)
              (http/stop st))
+           (prn :starting)
            (-> service-map
                http/default-interceptors
                http/create-server
-               http/start))))
-
+               http/start)
+           (prn :started))))
