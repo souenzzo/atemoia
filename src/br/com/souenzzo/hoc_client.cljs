6a19006af7df2b49afffb2bfe39283f4e0653cf1
Enzzo Cavallo <souenzzo@gmail.com>
2020-07-16 22:32:39 -0300

    PreventDefault

--- src/br/com/souenzzo/hoc_client.cljs
+++ src/br/com/souenzzo/hoc_client.cljs
@@ -4,17 +4,24 @@
             [goog.object :as gobj]
             [goog.dom :as gdom]
             [com.wsscode.pathom.connect :as pc]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [cljs.core.async.interop :as async.p]
+            [clojure.core.async :as async]
+            [edn-query-language.core :as eql]))
+
 
 (defn hoc-form
   [{::keys [sym]}]
   (fn
     ([env] (vec (::pc/params (pc/mutation-data env sym))))
-    ([env tree]
+    ([{::keys [remote]} tree]
      (r/createElement
        "form"
-       #js {:action (str "/" sym)
-            :method "POST"}
+       #js {:action   (str "/" sym)
+            :onSubmit (fn [e]
+                        (.preventDefault e)
+                        (async/put! remote `[(~sym {})]))
+            :method   "POST"}
        (r/createElement "input"
                         #js{:type  "submit"
                             :value (str sym)})))))
@@ -54,8 +61,10 @@
   (let [[{:>/keys [inc-form
                    table-counter]}
          setState] (r/useState nil)
-        query [{:>/inc-form (ui-inc-form {})}
-               {:>/table-counter (ui-table-counter {})}]
+        remote (async/chan)
+        env {::remote remote}
+        query [{:>/inc-form (ui-inc-form env)}
+               {:>/table-counter (ui-table-counter env)}]
         fetch! (fn []
                  (-> (js/fetch "/api"
                                #js{:method "POST"
@@ -64,12 +73,35 @@
                               (.text e)))
                      (.then (fn [text]
                               (setState (edn/read-string text))))))]
+    (async/go-loop
+      []
+      (let [{:keys [dispatch-key]
+             :as   op} (-> remote
+                           async/<!
+                           eql/query->ast
+                           :children
+                           first)
+            body (-> {:type     :root
+                      :children [(merge (eql/query->ast query)
+                                        op)]}
+                     eql/ast->query
+                     pr-str)
+            next-state (-> (js/fetch "/api" (js-obj "method" "POST"
+                                                    "body" body))
+                           async.p/<p!
+                           .text
+                           async.p/<p!
+                           edn/read-string
+                           (get dispatch-key))]
+        (setState next-state)
+        (recur)))
+
     (r/createElement "div"
                      nil
                      (r/createElement "button" #js{:onClick fetch!}
                                       "fetch!")
-                     (ui-inc-form {} inc-form)
-                     (ui-table-counter {} table-counter))))
+                     (ui-inc-form env inc-form)
+                     (ui-table-counter env table-counter))))
 
 (def app (r/createElement hello))
 
