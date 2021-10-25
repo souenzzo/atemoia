b42d127e7079ffc2d7079c1dee58881585f94b88
Enzzo Cavallo <souenzzo@gmail.com>
2021-10-25 08:14:55 -0300

    Rollback atemoia.client

--- src/atemoia/client.cljs
+++ src/atemoia/client.cljs
@@ -2,27 +2,64 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rd]))
 
-(defonce *state
-  (r/atom {:todos []}))
+(defonce state (r/atom {}))
+
+(defn fetch-todos
+  []
+  (-> (js/fetch "/todo")
+    (.then (fn [response]
+             (when-not (.-ok response)
+               (throw (ex-info (.-statusText response)
+                        {:response response})))
+             (swap! state dissoc :error)
+             (.json response)))
+    (.then (fn [todos]
+             (swap! state assoc :todos (js->clj todos
+                                         :keywordize-keys true))))
+    (.catch (fn [ex]
+              (swap! state assoc :error (ex-message ex))))))
 
 (defn ui-root
   []
-  (let [{:keys [todos]} @*state]
+  (let [{:keys [error todos]} @state]
     [:div
+     [:p "This is a sample clojure app to demonstrate how to use "
+      [:a {:href "https://clojure.org/guides/tools_build"}
+       "tools.build"]
+      " to create and deploy a full-stack clojure app."]
+     [:p "Checkout our "
+      [:a {:href "https://github.com/souenzzo/atemoia"}
+       "README"]]
      [:form
       {:on-submit (fn [^js evt]
                     (.preventDefault evt)
                     (let [el (-> evt
                                .-target
                                .-elements
-                               .-note)]
-                      (swap! *state update :todos (fn [todos]
-                                                    (conj todos
-                                                      {:todo/id   (count todos)
-                                                       :todo/note (.-value el)})))
-                      (set! (.-value el) "")))}
+                               .-note)
+                          json-body #js{:note (.-value el)}
+                          unlock (fn [success?]
+                                   (fetch-todos)
+                                   (when success?
+                                     (set! (.-value el) ""))
+                                   (set! (.-disabled el) false))]
+                      (set! (.-disabled el) true)
+                      (-> (js/fetch "/todo" #js{:method "POST"
+                                                :body   (js/JSON.stringify json-body)})
+                        (.then (fn [response]
+                                 (unlock (.-ok response))))
+                        (.catch (fn [ex]
+                                  (unlock false))))))}
       [:label
-       "note: " [:input {:name "note"}]]]
+       "note: "
+       [:input {:name "note"}]]]
+     (when error
+       [:<>
+        [:pre (str error)]
+        [:button {:on-click (fn []
+                              (js/fetch "/install-schema"
+                                #js{:method "POST"}))}
+         "install schema"]])
      [:ul
       (for [{:todo/keys [id note]} todos]
         [:li {:key id}
@@ -31,7 +68,8 @@
 (defn start
   []
   (some->> (js/document.getElementById "atemoia")
-    (rd/render [ui-root])))
+    (rd/render [ui-root]))
+  (fetch-todos))
 
 (defn after-load
   []
