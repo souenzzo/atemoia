12fd505723751b15b87de595c6ff0901f2fe4281
Enzzo Cavallo <souenzzo@gmail.com>
2022-06-19 21:59:15 -0300

    Removal and cleanup

--- src/atemoia/server.clj
+++ src/atemoia/server.clj
@@ -8,9 +8,7 @@
             [io.pedestal.http.route :as route]
             [io.pedestal.interceptor :as interceptor]
             [next.jdbc :as jdbc])
-  (:import (java.net URI)
-           (org.eclipse.jetty.server.handler.gzip GzipHandler)
-           (org.eclipse.jetty.servlet ServletContextHandler)))
+  (:import (java.net URI)))
 
 (set! *warn-on-reflection* true)
 
@@ -30,8 +28,7 @@
 
 (defn index
   [_]
-  (let [html [:html
-              {:lang "en"}
+  (let [html [:html {:lang "en"}
               [:head
                [:meta {:charset "UTF-8"}]
                [:link {:rel "icon" :href "data:"}]
@@ -43,10 +40,8 @@
                        :content "A simple full-stack clojure app"}]
                [:title "atemoia"]]
               [:body
-               {:style {:min-height "100%"}}
                [:div {:id "atemoia"} "loading ..."]
-               [:script
-                {:src "/atemoia/main.js"}]]]]
+               [:script {:src "/atemoia/main.js"}]]]]
     {:body    (->> html
                 (h/html {:mode :html})
                 (str "<!DOCTYPE html>\n"))
@@ -87,8 +82,8 @@
      ["/install-schema" :post install-schema]})
 
 (defn create-service
-  [env]
-  (-> env
+  [service-map]
+  (-> service-map
     (assoc ::http/secure-headers {:content-security-policy-settings ""}
            ::http/routes (fn []
                            (route/expand-routes routes)))
@@ -97,34 +92,26 @@
       (partial cons
         (interceptor/interceptor {:enter (fn [ctx]
                                            (-> ctx
-                                             (update :request merge env)))})))))
+                                             (update :request merge service-map)))})))))
 
 (defonce state
   (atom nil))
 
 (defn -main
   [& _]
-  (let [port (-> (or (System/getenv "PORT")
-                   "8080")
-               parse-long)
-        database-url (or (System/getenv "DATABASE_URL")
-                       "postgres://postgres:postgres@127.0.0.1:5432/postgres")
+  (let [env (System/getenv)
+        port (parse-long (get env "PORT" "8080"))
+        database-url (get env "DATABASE_URL" "postgres://postgres:postgres@127.0.0.1:5432/postgres")
         atm-conn-jdbc-url (database->jdbc-url database-url)]
     (swap! state
       (fn [st]
         (some-> st http/stop)
-        (-> {::http/port              port
-             ::atm-conn               {:jdbcUrl atm-conn-jdbc-url}
-             ::http/file-path         "target/classes/public"
-             ::http/host              "0.0.0.0"
-             ::http/type              :jetty
-             ::http/join?             false
-             ::http/container-options {:context-configurator (fn [^ServletContextHandler context]
-                                                               (let [gzip-handler (GzipHandler.)]
-                                                                 (.addIncludedMethods gzip-handler (into-array ["GET" "POST"]))
-                                                                 (.setExcludedAgentPatterns gzip-handler (make-array String 0))
-                                                                 (.setGzipHandler context gzip-handler))
-                                                               context)}}
+        (-> {::http/port      port
+             ::atm-conn       {:jdbcUrl atm-conn-jdbc-url}
+             ::http/file-path "target/classes/public"
+             ::http/host      "0.0.0.0"
+             ::http/type      :jetty
+             ::http/join?     false}
           create-service
           http/dev-interceptors
           http/create-server
