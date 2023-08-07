8a724d6120f42fbd9c4c0f71138a15a0c6ffe072
Enzzo Cavallo <souenzzo@gmail.com>
2023-08-07 06:11:11 -0300

    Add static file server

--- src/atemoia/node_server.cljs
+++ src/atemoia/node_server.cljs
@@ -1,6 +1,9 @@
 (ns atemoia.node-server
-  (:require [com.fulcrologic.fulcro.dom :as dom]
+  (:require [clojure.string :as string]
+            [com.fulcrologic.fulcro.dom :as dom]
+            ["node:fs/promises" :as fs]
             ["node:http" :as http]
+            ["node:url" :as url]
             ["react" :as r]
             ["react-dom/server" :as rds]))
 
@@ -8,20 +11,58 @@
 ;; see https://book.fulcrologic.com/#RawReactHooks
 
 (defn Root []
-  (dom/html
+  (dom/html {:lang "en"}
     (dom/head
-      (dom/title "hello"))
+      (dom/meta {:charSet "UTF-8"})
+      (dom/link {:rel "icon" :href "data:"})
+      (dom/meta {:name    "viewport"
+                 :content "width=device-width, initial-scale=1.0"})
+      (dom/meta {:name    "theme-color"
+                 :content "#000000"})
+      (dom/meta {:name    "description"
+                 :content "A simple full-stack clojure app"})
+      (dom/title "atemoia"))
     (dom/body
-      (dom/div "world"))))
+      (dom/div {:id "atemoia"} "loading ...")
+      (dom/script {:src "/atemoia/main.js"}))))
+
+(defn fs-handler
+  [prefix]
+  (fn [{:keys [uri]}]
+    (-> (fs/readFile (str prefix uri))
+      (.then (fn [x]
+               {:status 200
+                :body   x})))))
+
+(def static-handler
+  (fs-handler "./target/classes/public"))
 
 (defn handler-impl
-  [req ^js res]
-  (.writeHead res 200 #js{"Content-Type" "text/html"})
-  (.end res (str "<!DOCTYPE html>\n"
-              (rds/renderToString (r/createElement Root)))))
+  [req]
+  (-> (static-handler req)
+    (.catch (fn [_err]
+              {:headers {"Content-Type" "text/html"}
+               :body    (str "<!DOCTYPE html>\n"
+                          (rds/renderToString (r/createElement Root)))
+               :status  200}))))
 
 (defn start
   [& _]
-  (.listen (http/createServer (fn [req res]
-                                (handler-impl req res)))
+  (.listen (http/createServer (fn [req ^js res]
+                                (let [url (url/parse (.-url req))]
+                                  (-> {:uri            (.-pathname url)
+                                       :query-string   (.-query url)
+                                       :request-method (keyword (string/lower-case (.-method req)))
+                                       :headers        (js->clj (.-headers req))
+                                       #_#_:server-port -1
+                                       #_#_:server-name "localhost"
+                                       #_#_:remote-addr "127.0.0.1"
+                                       #_#_:scheme :http
+                                       #_#_:protocol "HTTP/1.1"
+                                       #_#_:body nil}
+                                    handler-impl
+                                    js/Promise.resolve
+                                    (.then (fn [{:keys [status body headers]}]
+                                             (.writeHead res status (clj->js headers))
+                                             (.end res body)))))))
     3000))
